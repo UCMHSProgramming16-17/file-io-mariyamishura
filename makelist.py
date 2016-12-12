d1bd19b3644975b518ca86f1a754930664a115ee
Mariya Mishura <mmishura@ucvts.org>
2016-12-12 18:20:54 +0000

    added list

---
+++ makelist.py
@@ -0,0 +1,8 @@
+sequence = []sequence = []
+s_length = 9 #get to ten items
+count = 0
+while len(sequence) < 10: #set length of sequence\items in list
+int(input("pick a number"))
+sequence.append(new_val) #add a value to the end of the list
+for number in sequence:
+    print(number)
